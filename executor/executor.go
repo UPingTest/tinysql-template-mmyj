997fb4250701b6c3c1b033f6f18b8e547fc1a5e4
mmyj <mayj.hello@gmail.com>
2020-05-19 13:56:33 +0800

    temp

--- executor/executor.go
+++ executor/executor.go
@@ -389,11 +389,19 @@ func (e *SelectionExec) Next(ctx context.Context, req *chunk.Chunk) error {
 			1. the `req` chunk` is full.
 			2. there is no further results from child.
 			3. meets any error.
-	 */
+	*/
 	for {
+		if req == nil {
+			break
+		}
 		// Fill in the `req` util it is full or the `inputIter` is fully processed.
 		for ; e.inputRow != e.inputIter.End(); e.inputRow = e.inputIter.Next() {
 			// Your code here.
+			if e.selected[e.inputRow.Idx()] {
+				req.AppendRow(e.inputRow)
+			} else {
+				req.AppendNull(e.inputRow.Idx())
+			}
 		}
 		err := Next(ctx, e.children[0], e.childResult)
 		if err != nil {
@@ -405,8 +413,13 @@ func (e *SelectionExec) Next(ctx context.Context, req *chunk.Chunk) error {
 		}
 		/* Your code here.
 		   Process and filter the child result using `expression.VectorizedFilter`.
-		 */
+		*/
+		e.selected, err = expression.VectorizedFilter(e.ctx, e.filters, chunk.NewIterator4Chunk(e.childResult), e.selected)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 // unBatchedNext filters input rows one by one and returns once an input row is selected.
